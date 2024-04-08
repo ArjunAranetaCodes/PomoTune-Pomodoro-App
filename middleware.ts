4d695294ffdea49c467a7e4db28621383eefae5d
MoreCodes <arjunaraneta@gmail.com>
2024-04-08 22:44:00 +1000

    updated webhooks folder

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes:['/api/webhook/clerk', '/api/webhook/clerk/route.ts'],
+  publicRoutes:["/api/webhooks(.*)"],
 });
 
 export const config = {
