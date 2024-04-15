3a5916e023441333b4242e1b9c8eb1299aa427eb
MoreCodes <arjunaraneta@gmail.com>
2024-04-15 15:43:34 +1000

    added fields

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -59,6 +59,7 @@ export async function POST(req: Request) {
 
   // CREATE
   if (eventType === "user.created") {
+    console.log("User Created")
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
--- models/UserModel.ts
+++ models/UserModel.ts
@@ -2,12 +2,22 @@ import mongoose from "mongoose";
 
 export interface UserModel extends mongoose.Document {
   clerkId: String;
+  email: String;
+  username?: String;
 }
 
 const UserModelSchema = new mongoose.Schema<UserModel>({
   clerkId: {
     type: String,
     unique: true,
+  },
+  email: {
+    type: String,
+    unique:true,
+  },
+  username: {
+    type: String,
+    unique:true,
   },
 });
 
